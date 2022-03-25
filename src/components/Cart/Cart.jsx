ecd4ae1f3dbffb78adc949599cc2eeb8422569b0
Mehedi Hassan <rehan51322@gmail.com>
2022-03-26 03:40:06 +0600

    item added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart}) => {
-   let name;
-   for(const product of cart){
-      name = product.name;
-   }
+   
    return (
       <div className='cart'>
          <h2>Selected Toy's</h2>
-         <p>{name}</p>
+         {
+            cart.map(item =>(
+               <h4>{item.name}</h4>
+            ))
+         }
       </div>
    );
 };
